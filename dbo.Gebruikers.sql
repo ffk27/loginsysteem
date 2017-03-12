94fee1229ea1b3189a8c25b6f3a0c05d6c487889
ffk27 <fpfolkertsma@gmail.com>
2017-03-12 20:01:59 +0100

    db

---
+++ dbo.Gebruikers.sql
@@ -0,0 +1,12 @@
+﻿/* Microsoft SQL Server */
+CREATE TABLE [dbo].[Gebruikers] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [naam]           NVARCHAR (45)  NOT NULL,
+    [gebruikersnaam] NVARCHAR (30)  NOT NULL,
+    [niveau]         INT            NOT NULL,
+    [email]          NVARCHAR (254) NOT NULL,
+    [salt]           CHAR(10)  NOT NULL,
+    [hash]           CHAR(64)  NOT NULL,
+    [aanmelddatum]   DATETIME       DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
